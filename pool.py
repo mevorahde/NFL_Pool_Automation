b6ee73959ac700e83a129b6e185947fd6806c430
David Mevorah <mevorahde@hotmail.com>
2020-10-03 22:10:43 -0700

    Update pool.py on pulling spreads.

--- pool.py
+++ pool.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re
 
-
 # Load the webpage content
 url = "https://www.scoresandodds.com/las-vegas-odds"
 r = requests.get(url)
@@ -10,9 +9,24 @@ r = requests.get(url)
 # Convert to a beautiful soup object
 webpage = bs(r.content, "html.parser")
 
-# Get the table of
-table = webpage.select("table.segmented tbody")[0]
-columns = table.find("thread").find("th", string=re.compile("l|Line"))
+# Get the table for Column names
+table = webpage.select("table.segmented")[0]
+columns = table.find("td", attrs={"id": "betting-trends--nfl"}).find_all("th", text=re.compile("Line"))
 column_names = [c.string for c in columns]
 
-print(columns)
+print(column_names)
+
+
+l = []
+spread_rows = columns = table.find("td", attrs={"id": "betting-trends--nfl"}).find_all("span", text=re.compile("-"))
+spreads = [str(s.get_text()).strip() for s in spread_rows]
+print(spreads)
+
+
+# for tr in table_rows:
+#     td = tr.find_all("td")
+#     row = [str(tr.get_text()).strip() for tr in td]
+#     l.append(row)
+
+# print(table.prettify())
+
